30c668fd92338303ffc3a14611812cf28e792cf2
HyunGeunK <hyungeun0313@naver.com>
2025-08-07 16:34:09 +0900

    중간저장

---
+++ src/mylab/library/entity/Book.java
@@ -0,0 +1,65 @@
+package mylab.library.entity;
+
+public class Book {
+	private String title;
+	private String author;
+	private String isbn;
+	private int publishYear;
+	private boolean isAvailable;
+	
+	public Book(boolean isAvailable) {
+		super();
+		this.isAvailable = isAvailable;
+	}
+
+	public Book(String title, String author, String isbn, int publishYear) {
+		super();
+		this.title = title;
+		this.author = author;
+		this.isbn = isbn;
+		this.publishYear = publishYear;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	public String getIsbn() {
+		return isbn;
+	}
+
+	public void setIsbn(String isbn) {
+		this.isbn = isbn;
+	}
+
+	public int getPublishYear() {
+		return publishYear;
+	}
+
+	public void setPublishYear(int publishYear) {
+		this.publishYear = publishYear;
+	}
+
+	public boolean isAvailable() {
+		return isAvailable;
+	}
+
+	public void setAvailable(boolean isAvailable) {
+		this.isAvailable = isAvailable;
+	}
+	
+	
+	
+}
